5f250a540ae8b7404d93c091e6948e4925efaf4f
Aidan Quist <acquist@mtu.edu>
2023-03-21 14:33:58 -0400

    added footer to fxml file. Error with java 19 need to redo

--- src/MP3Application.java
+++ src/MP3Application.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -9,7 +10,9 @@ public class MP3Application extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("test-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 480);
+//        Font.loadFont(getClass().getResourceAsStream("fonts/RobotoMono-Regular.ttf"), 16);
+        //scene.getStylesheets().add(getClass().getResource("MP3.css").toExternalForm());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/components/PlayButton.java
@@ -0,0 +1,10 @@
+package components;/*
+ * PROGRAM: PlayButton
+ * AUTHOR: Aidan Quist
+ * COURSE: CS 1131
+ * LAB: L01
+ */
+
+public class PlayButton {
+
+}
