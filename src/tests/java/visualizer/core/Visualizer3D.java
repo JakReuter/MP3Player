54b0e35ffe23ffc9e7a896316e9d80ddfae3f78f
Brendan Reuter <breuterrox@gmail.com>
2023-04-17 00:31:06 -0400

    3d implement w/ 3dscene

--- src/tests/java/visualizer/core/Visualizer3D.java
+++ src/tests/java/visualizer/core/Visualizer3D.java
@@ -3,21 +3,112 @@ package visualizer.core;
 import MP3Player.mp3Player.visualizer.core.Series;
 import MP3Player.mp3Player.visualizer.core.Visualizer;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import org.junit.Test;
 
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Mesh;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
+
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Visualizer3D extends Visualizer {
     private Point3d[][] points;
     private int numOfColumns;
+    private SubScene subScene;
+    private Group subSceneRoot;
+    private Group objectGroup;
+    private double sceneWidth = 400;
+    private double sceneHeight = 400;
+    //private double sceneMaxDimension = Math.sqrt((sceneWidth*sceneWidth+sceneHeight*sceneHeight)/2);
+
+    private double mousePosX;
+    private double mousePosY;
+    private double mouseOldX;
+    private double mouseOldY;
+    private double mouseDeltaX;
+    private double mouseDeltaY;
+
+    /**
+     * Camera for 3d depth scene
+     */
+    private PerspectiveCamera camera;
+    /**
+     * We want the camera to be fixed at the origin so
+     * we create a group at the origin and attach
+     * the camera at a distance behind the plane
+     *
+     */
+    private TransformableGroup cameraPlaneGroup;
+    private final double CAMERA_NEAR_CLIP = 0.1;
+    private final double CAMERA_FAR_CLIP = 10000;
+    private final double CAMERA_DISTANCE = -1100;
 
 
-    protected Pane childRoot;
+    protected Pane rootOfDisplay;
 
+    /**
+     * A group that holds 3 rotation transforms and
+     *  a scalar, which allows easier manipulation
+     *  of the transformation of a node without
+     *  indexing node.getTransforms()
+     */
+    private class TransformableGroup extends Group {
+
+        public Rotate rx = new Rotate();
+        public Rotate ry = new Rotate();
+        public Rotate rz = new Rotate();
+        public Scale s = new Scale();
+
+        public TransformableGroup() {
+            super();
+            rx.setAxis(Rotate.X_AXIS);
+            ry.setAxis(Rotate.Y_AXIS);
+            rz.setAxis(Rotate.Z_AXIS);
+            getTransforms().addAll(rx, ry, rz, s);
+        }
+    }
+    public class Series3d extends Series {
+
+        public Series3d(Number[] arr, int size, Visualizer vis) {
+            super(arr, size, vis);
+        }
+        public class Data3d extends Series.Data {
+
+            private Number zValue;
+            private ObjectProperty<Number> zPosition;
+
+            public Data3d(Number x, Number y) {
+                this(x, y, 0);
+            }
+
+            public Data3d(Number x, Number y, Number z) {
+                super(x, y);
+                zPosition = new SimpleObjectProperty<Number>(this, "yPosition");
+            }
+
+            //Animation Properties
+            public Number getzPosition() {
+                return zPosition.get();
+            }
+
+            public void setzPosition(Number value) {
+                zPosition.set(value);
+            }
+
+            public ObjectProperty<Number> zPositionProperty() {
+                return zPosition;
+            }
+        }
+    }
     public class Point3d{
         private double scalarFromCamPos;
         private double[] coords3d;
@@ -96,12 +187,114 @@ public class Visualizer3D extends Visualizer {
 
     public Visualizer3D(int bands, String name) {
         super(bands,name);
-        childRoot = new Pane();
+        rootOfDisplay = new Pane();
+        objectGroup = new Group();
     }
 
     @Override
     protected void adjustRange(int outLier) {
+
     }
+
+
+    public Scene buildScene() {
+        Scene mainScene = new Scene(rootOfDisplay, sceneWidth, sceneHeight);
+        buildSubScene();
+        return mainScene;
+    }
+
+    public void buildSubScene() {
+        subSceneRoot = new Group();
+        subScene = new SubScene(subSceneRoot, sceneWidth, sceneHeight, true, SceneAntialiasing.BALANCED);
+        subSceneRoot.getChildren().addAll(objectGroup);
+        buildCamera();
+        rootOfDisplay.getChildren().addAll(subScene);
+        handleMouse(subScene);
+    }
+
+    /**
+     * Builds and initializes the perspective Camera
+     */
+    private void buildCamera() {
+        camera = new PerspectiveCamera(true);
+        subScene.setCamera(camera);
+        cameraPlaneGroup = new TransformableGroup();
+        subSceneRoot.getChildren().add(cameraPlaneGroup);
+        cameraPlaneGroup.getChildren().add(camera);
+        cameraPlaneGroup.rz.setAngle(0);
+
+        camera.setNearClip(CAMERA_NEAR_CLIP);
+        camera.setFarClip(CAMERA_FAR_CLIP);
+        camera.setTranslateZ(CAMERA_DISTANCE);
+        cameraPlaneGroup.ry.setAngle(0);
+        cameraPlaneGroup.rx.setAngle(0);
+    }
+    /**
+     * Sets up mouse handlers for various mouse triggered events.
+     * @param scene	Draw3D Scene Group
+     */
+    private void handleMouse(SubScene scene) {
+        scene.setOnScroll(new EventHandler<ScrollEvent>() {
+            @Override public void handle(ScrollEvent se) {
+                objectGroup.setScaleX(objectGroup.getScaleX() + se.getDeltaY()*.01);
+                objectGroup.setScaleY(objectGroup.getScaleY() + se.getDeltaY()*.01);
+                objectGroup.setScaleZ(objectGroup.getScaleZ() + se.getDeltaY()*.01);
+            }
+        });
+
+//		scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//          @Override public void handle(MouseEvent me) {
+//            	if (me.getClickCount() == 1) {
+//                    clearView();
+//              }
+//          }
+//      });
+
+//        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//            @Override public void handle(MouseEvent me) {
+//                if (me.getClickCount() > 1) {
+//                    reset();
+//                }
+//            }
+//        });
+
+        scene.setOnMousePressed(me -> {
+            mousePosX = me.getSceneX();
+            mousePosY = me.getSceneY();
+            mouseOldX = me.getSceneX();
+            mouseOldY = me.getSceneY();
+        });
+
+        scene.setOnMouseDragged(me -> {
+            mouseOldX = mousePosX;
+            mouseOldY = mousePosY;
+            mousePosX = me.getSceneX();
+            mousePosY = me.getSceneY();
+            mouseDeltaX = -(mousePosX - mouseOldX);
+            mouseDeltaY = -(mousePosY - mouseOldY);
+
+            double modifier = .2;
+
+            if (me.isPrimaryButtonDown()) {
+                cameraPlaneGroup.ry.setAngle(cameraPlaneGroup.ry.getAngle() - mouseDeltaX*modifier);
+                cameraPlaneGroup.rx.setAngle(cameraPlaneGroup.rx.getAngle() + mouseDeltaY*modifier);
+//                   System.out.println("X= " + cameraXform.rx.getAngle() +
+//                    		", Y= " + cameraXform.ry.getAngle() +
+//                    		", Z= " + cameraXform.rz.getAngle());
+            }
+        });
+    }
+
+    public void manuallyAdd3dShape(Shape3D newShape){
+        System.out.println("Adding shape:" + newShape.toString()+" at x:"+newShape.getTranslateX()+" y:"+newShape.getTranslateY()+" z:"+newShape.getTranslateZ());
+        objectGroup.getChildren().add(newShape);
+    }
+
+    public void manuallyAdd3dShape(Collection<? extends Shape3D> list){
+        //System.out.println("Adding shape:" + newShape.toString()+" at x:"+newShape.getTranslateX()+" y:"+newShape.getTranslateY()+" z:"+newShape.getTranslateZ());
+        objectGroup.getChildren().addAll(list);
+    }
+
 
 
     //test
@@ -112,6 +305,10 @@ public class Visualizer3D extends Visualizer {
     //test
     public double getCoordFromIndex(double index){
         return (400/numOfColumns)*index;
+    }
+    //test
+    public double getCoordFromIndex(double index,int max){
+        return (400/max)*index;
     }
 
     //Test
@@ -144,7 +341,7 @@ public class Visualizer3D extends Visualizer {
                 Point3d start = points[xVal][yVal];
                 Point3d end = points[xVal+1][yVal];
                 Line newLine = new Line(start.getX(),start.getY(),end.getX(),end.getY());
-                childRoot.getChildren().add(newLine);
+                rootOfDisplay.getChildren().add(newLine);
                 newLine.setLayoutX(0);
                 newLine.setLayoutY(0);
                 out.add(newLine);
@@ -161,7 +358,7 @@ public class Visualizer3D extends Visualizer {
                 Point3d start = points[xVal][yVal];
                 Point3d end = points[xVal][yVal+1];
                 Line newLine = new Line(start.getX(),start.getY(),end.getX(),end.getY());
-                childRoot.getChildren().add(newLine);
+                rootOfDisplay.getChildren().add(newLine);
                 newLine.setLayoutX(0);
                 newLine.setLayoutY(0);
                 out.add(newLine);
@@ -177,11 +374,12 @@ public class Visualizer3D extends Visualizer {
         getXLine();
         //childRoot.getChildren().addAll(getXLine());
        // childRoot.getChildren().addAll(getYLine());
-        childId=childRoot.toString();
-        childRoot.setMinHeight(400);
-        childRoot.setMinWidth(400);
-        this.getRoot().getChildren().add(childRoot);
+        childId= rootOfDisplay.toString();
+        rootOfDisplay.setMinHeight(400);
+        rootOfDisplay.setMinWidth(400);
+        this.getRoot().getChildren().add(rootOfDisplay);
 
     }
+
 
 }
---
+++ src/tests/java/visualizer/tests/Vis3dTest.java
@@ -0,0 +1,196 @@
+package visualizer.tests;
+
+import com.sun.javafx.fxml.builder.TriangleMeshBuilder;
+import javafx.application.Application;
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableDoubleValue;
+import javafx.collections.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.*;
+import javafx.stage.Stage;
+import sun.java2d.pipe.SpanShapeRenderer;
+import visualizer.core.Visualizer3D;
+
+import java.util.ArrayList;
+
+public class Vis3dTest extends Application {
+    Visualizer3D vizzy;
+    Shape3D testObject;
+    ArrayList<Shape3D> shapes;
+    int dim;
+    SimpleDoubleProperty scalar;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        vizzy = new Visualizer3D(11, "3d");
+        shapes = new ArrayList<>();
+        scalar = new SimpleDoubleProperty(25);
+        //SphereTest();
+        PointFieldTest(65);
+        MeshBuilderTest();
+        Scene scene = vizzy.buildScene();
+        handleKey(scene);
+        stage.setTitle("test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void GridTest(){
+    }
+    public void SphereTest(){
+        testObject = new Sphere(1);
+        vizzy.manuallyAdd3dShape(testObject);
+    }
+    public void CubeTest(){
+        testObject = new Box(200,200,200);
+        //newSphere.setHeight(50);
+        vizzy.manuallyAdd3dShape(testObject);
+    }
+    public void PointFieldTest(int dimension){
+        dim=dimension;
+        for(int i = 0; i<dimension;i++){
+            for(int j = 0; j<dimension;j++){
+                Sphere dot = new Sphere(1);
+                double xCoord = vizzy.getCoordFromIndex(i,dimension)-200;
+                double zCoord = vizzy.getCoordFromIndex(j,dimension)-200;
+                //double yCoord = 25*Math.sin(.1*Math.sqrt(xCoord*xCoord+zCoord*zCoord));
+                double distFromOrigin = Math.sqrt(xCoord*xCoord+zCoord*zCoord);
+                DoubleBinding binding = new DoubleBinding() {
+                    {
+                        super.bind(scalar);
+                    }
+                    @Override
+                    protected double computeValue() {
+                        return scalar.doubleValue()*Math.sin(.1*distFromOrigin);
+                    }
+                };
+                dot.translateYProperty().bind(binding);
+                dot.setTranslateX(xCoord);
+                dot.setTranslateZ(zCoord);
+                //dot.setTranslateY(yCoord);
+                shapes.add(dot);
+            }
+        }
+
+        //vizzy.manuallyAdd3dShape(shapes);
+        //newSphere.setHeight(50);
+    }
+
+    public void MeshTest(){
+        for(int xC = 0; xC<2;xC++){
+            for(int yC = 0; yC<2;yC++){
+                Sphere dot = new Sphere(5);
+                dot.setTranslateX(xC*50);
+                dot.setTranslateY(yC*50);
+                dot.setTranslateZ(0);
+                shapes.add(dot);
+            }
+        }
+        TriangleMesh mesh = new TriangleMesh();
+        mesh.getPoints().addAll(
+                0,0,0,
+                0,50,0,
+                50,0,0,
+                50,50,0
+        );
+        mesh.getTexCoords().addAll(
+                0f,0f,
+                0f,1f,
+                1f,0f,
+                1f,1f
+        );
+        mesh.getFaces().addAll(
+                0,0,2,2,1,1,
+                1,1,2,2,3,3
+        );
+        vizzy.manuallyAdd3dShape(shapes);
+        MeshView test = new MeshView(mesh);
+        test.setMaterial(new PhongMaterial(Color.FIREBRICK));
+        vizzy.manuallyAdd3dShape(test);
+    }
+    public void MeshBuilderTest(){
+        TriangleMesh newMesh = new TriangleMesh();
+        ObservableFloatArray points = FXCollections.observableFloatArray();
+        ObservableFloatArray textures = FXCollections.observableFloatArray();
+        ObservableIntegerArray faces = FXCollections.observableIntegerArray();
+        shapes.get(0).setMaterial(new PhongMaterial(Color.GREEN));
+        for (int i = 0; i < shapes.size(); i++) {
+                Shape3D dot1 = shapes.get(i);
+                //Shape3D dot2 = shapes.get(dim + k+i);
+
+                float f1X = (float) dot1.getTranslateX();
+                float f1Y = (float) dot1.getTranslateY();
+                float f1Z = (float) dot1.getTranslateZ();
+                points.addAll(f1X, f1Y, f1Z);
+            int finalI = i;
+            dot1.translateYProperty().addListener((observable, oldValue, newValue) -> {
+                    newMesh.getPoints().set(finalI *3+1,newValue.floatValue());
+                });
+
+                textures.addAll(buildTextures(f1X, f1Z));
+                if ((i > dim)/*&&((i-dim)%2)==1*/) {
+                    int j = i - dim;
+                    faces.addAll(
+                            j-1,j-1,i-1,i-1,j,j,
+                            i-1,i-1,i,i,j,j);
+                }
+            }
+
+        newMesh.getPoints().addAll(points);
+        newMesh.getTexCoords().addAll(textures);
+        newMesh.getFaces().addAll(faces);
+        MeshView test = new MeshView(newMesh);
+
+        test.setMaterial(new PhongMaterial(Color.FIREBRICK));
+        vizzy.manuallyAdd3dShape(test);
+    }
+    protected float[] buildTextures(float... in){
+        float[] out = new float[in.length];
+        for(int i = 0; i<out.length;i++){
+            out[i]=((in[i]+200)/400);
+        }
+        return out;
+    }
+    public void PathTest(){
+        for(int i=0;i< dim/*shapes.size()-dim-1*/; i++){
+            Path newPath = new Path();
+            MoveTo firstMove = new MoveTo(0,0);
+            LineTo line = new LineTo();
+
+        }
+    }
+    public void ConnectPointTest(){
+        ArrayList<Shape3D> lines = new ArrayList<>();
+        for(int i = 0; i<dim;i++){
+            for(int j = 0; j<dim;j++){
+
+            }
+        }
+    }
+
+    public void handleKey(Scene scene){
+        scene.setOnKeyPressed( keyEvent -> {
+            KeyCode code = keyEvent.getCode();
+            switch(code){
+                case UP:
+                    scalar.set(scalar.get()+1);
+                    System.out.println(scalar);
+                    break;
+                case DOWN:
+                    scalar.set(scalar.get()-1);
+                    System.out.println(scalar);
+                    break;
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
